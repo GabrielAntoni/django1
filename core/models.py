6e9c393b3d080ee1801bda4856b5b84dd498f01a
Gabriel <gams0901@gmail.com>
2024-04-17 15:11:27 -0300

    Models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Visitas(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
+    Numero = models.BigIntegerField('e-mail',max_length=14)
--- core/views.py
+++ core/views.py
@@ -4,7 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    Apresentação = {
+        'Nome': 'Nome: Gabriel Antoni de Moraes Souza',
+        'Idade': 'Idade: 25'
+    }
+    return render(request, 'index.html',Apresentação)
 
 
 def contato(request):
