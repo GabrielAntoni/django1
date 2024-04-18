6c3768f35410f75c3137a79a98d4e3e005967119
Gabriel <gams0901@gmail.com>
2024-04-18 01:13:37 -0300

    adicionando css

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+from django.template import loader
 
 from .models import Certificado
 # Create your views here.
@@ -17,3 +19,11 @@ def index(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def error404(request, ex):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
